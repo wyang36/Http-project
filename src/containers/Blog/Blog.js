a3dc584642ad6b00cd88fb83150c549e1d0e877e
wyang36 <wenqin.z.yang@gmail.com>
2018-04-03 13:11:51 -0400

    course update
    adding nested routing for full post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+//import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
 
     render() {
-        
+
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
+                                to="/posts"
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
                                 }}
-                            >Home</NavLink></li>
+                            >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -33,8 +34,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/*<Route path="/:id" exact component={FullPost} /> */}
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,8 +1,11 @@
 ﻿import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -11,7 +14,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        //this.setState({ selectedPostId: id });
+        this.props.history.push({ pathname: this.props.match.url + '/' + id });
     }
 
     componentDidMount() {
@@ -35,20 +39,27 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(
                 post => {
-                    return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}
-                    />
+                    return (
+                        //<Link to={'/' + post.id} key={post.id}>
+                        <Post
+                            key={post.id}
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}
+                        />
+                        //</Link>
+                    )
                 }
             );
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + "/:id"} exact component={FullPost} />
+            </div>           
         );
     }
 }
